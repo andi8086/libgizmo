edb944f3c16e8120d31113a75a47f42b097678b8
Andreas J. Reichel <webmaster@6th-dimension.com>
2020-09-03 17:39:00 +0200

    Initial commit
    Use kbuild system from
    https://github.com/embedded-it/kbuild-template
    bf468d463c48444818c0828b86e97be244c3ed5c
    
    Add initial code for library

---
+++ include/_gizmo.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2020 by Andreas J. Reichel <webmaster@6th-dimension.com>
+ *
+ * All rights reserved.
+ *
+ * For additional licensing information see COPYING.gizmo
+ */
+
+#ifndef __LIB_GIZMO_H__
+#define __LIB_GIZMO_H__
+
+#endif // __LIB_GIZMO_H__
---
+++ include/bt_rb.h
@@ -0,0 +1,5 @@
+#ifndef __BT_RB_H__
+#define __BT_RB_H__
+
+
+#endif // __BT_RB_H__
